751fb2d0a3ceaa018440fde4d0388f3cc4f1e497
LuciaRyan <lcryan06@gmail.com>
2022-11-14 12:41:06 +0100

    code finalized, but output doesn't show up in the browser. In progress

--- src/app.js
+++ src/app.js
@@ -4,18 +4,16 @@ console.log("Hallo daar");
 
 const countryCurrency = (currency) => {
     let localCurrency = "";
-    for(let i= 0; i< currency.length; i++) {
-        if ( i > 0) {
+    for (let i = 0; i < currency.length; i++) {
+        if (i > 0) {
             localCurrency += "and" + localCurrency[i].name + "s."
-        }
-        else {
+        } else {
             localCurrency = localCurrency[i].name;
         }
     }
     console.log(localCurrency);
     return localCurrency
 }
-
 
 
 async function getCountryInformation() {
@@ -26,20 +24,29 @@ async function getCountryInformation() {
     const searchFieldOutput = searchField.value
     searchField.value = '';
 
-    try{
+    try {
         const response = await axios.get(BASE_URI + ENDPOINT);
         let countries = response.data
 
         const listOfCountries = document.getElementById('country-list')
             .map((country) =>
-            listOfCountries.innerHTML = `<li class = "list-item">
+                listOfCountries.innerHTML = `<li class = "list-items">
              <div class = "flag-item">
-                <h3 class = "country-name">${country.name}</h3>`
-
+                <h3 class = "country-name">${country.name}</h3>
+                <img src="${country.flags.png}" alt="${country.flag}" class ="image-size"/>
+</div>
+<h6 class="text-one">${country.name} is situated in ${country.region}. It has a population of ${country.population}</h6>
+<h6 class="text-two"> The capital city is ${country.capital} and you can pay with ${countryCurrency(country.currency)}</h6>
+</li>`
             )
 
     } catch (error) {
+        console.error(error)
 
     }
 
 }
+getCountryInformation();
+
+const searchButton = document.getElementById('search-button');
+searchButton.addEventListener('click', getCountryInformation)
