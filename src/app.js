6178d36369579712d0e6847fd7cfb376d280382f
LuciaRyan <lcryan06@gmail.com>
2022-11-14 14:25:59 +0100

    app.js still plays up while retrieving currency; controlling the function and variables

--- src/app.js
+++ src/app.js
@@ -2,51 +2,53 @@ import axios from 'axios';
 
 console.log("Hallo daar");
 
-const countryCurrency = (currency) => {
-    let localCurrency = "";
-    for (let i = 0; i < currency.length; i++) {
+/*
+const localCurrency = (currencies) => {
+    let local = "";
+    for (let i = 0; i < currencies.length; i++) {
         if (i > 0) {
-            localCurrency += "and" + localCurrency[i].name + "s."
+            local += ' and ' + localCurrency[i].name + 's. ';
         } else {
-            localCurrency = localCurrency[i].name;
+            local = localCurrency[i].name;
         }
     }
-    console.log(localCurrency);
-    return localCurrency
+    console.log(local);
+    return local;
 }
+*/
 
 
 async function getCountryInformation() {
-    const BASE_URI = 'https://restcountries.com';
-    const ENDPOINT = 'v2/all'
+    const BASE = "https://restcountries.com/";
+    const ENDPOINT = "v2/name/";
 
     const searchField = document.getElementById('search-field');
     const searchFieldOutput = searchField.value
     searchField.value = '';
 
     try {
-        const response = await axios.get(BASE_URI + ENDPOINT);
-        let countries = response.data
+        const response = await axios.get(BASE + ENDPOINT + searchFieldOutput);
+        let countryOutput = response.data
 
         const listOfCountries = document.getElementById('country-list')
-            .map((country) =>
-                listOfCountries.innerHTML = `<li class = "list-items">
-             <div class = "flag-item">
+        countryOutput.map((country) =>
+            listOfCountries.innerHTML = `<li class="list-items">
+             <div class= "flag-item">
                 <h3 class = "country-name">${country.name}</h3>
-                <img src="${country.flags.png}" alt="${country.flag}" class ="image-size"/>
+                <img src="${country.flags.png}" alt="${country.flag}" class="img-size"/>
 </div>
 <h6 class="text-one">${country.name} is situated in ${country.region}. It has a population of ${country.population}</h6>
-<h6 class="text-two"> The capital city is ${country.capital} and you can pay with ${countryCurrency(country.currency)}</h6>
+<h6 class="text-two"> The capital city is ${country.capital}.</h6>
 </li>`
-            )
+        )
 
-    } catch (error) {
-        console.error(error)
-
+    } catch (err) {
+        console.error(err)
     }
 
 }
+
 getCountryInformation();
 
 const searchButton = document.getElementById('search-button');
-searchButton.addEventListener('click', getCountryInformation)
+searchButton.addEventListener('click', getCountryInformation);
